99577825d5d6b0a6126294a10cee53ceb01d4700
changseop-lab <a01023853321@gmail.com>
2026-08-30 19:49:28 +0900

    Add files via upload

---
+++ 26031013_김창섭_자기이력서작성/26031013_김창섭_자기이력서작성.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26031013_김창섭_자기이력서작성
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("======자기이력서======\n ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("이름 : 김창섭\n ");
+            Console.Write("생년월일 : 070212\n ");
+            Console.Write("성별 : 남\n ");
+            Console.Write("나이 : (만)19세\n ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("==학력사항==\n ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" 신현초등학교 졸업\n ");
+            Console.Write(" 신현중학교 졸업\n ");
+            Console.Write(" 신현고등학교 졸업\n ");
+            Console.Write(" 한국it전문학교 재학\n ");
+
+
+        }
+    }
+}
